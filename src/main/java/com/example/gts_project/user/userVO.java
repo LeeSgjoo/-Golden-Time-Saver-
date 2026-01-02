2a4a7465e7778629258024eda2502aaa73cfa6ac
sg_joo_ <joo38219@gmail.com>
2026-01-02 16:34:47 +0900

    back/VODAO: create userVO, patientVO, hospitalVO

---
+++ src/main/java/com/example/gts_project/user/userVO.java
@@ -0,0 +1,52 @@
+package com.example.gts_project.user;
+
+
+/*
+FileName: userVO
+DB: user => PersonId, UserName, Password, UserType
+ */
+public class userVO {
+    private int personId;
+    private String userName;
+    private String password;
+    private int userType;
+
+    public userVO(){};
+    public userVO(String userName, String password, int userType){
+        this.userName = userName;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+}
