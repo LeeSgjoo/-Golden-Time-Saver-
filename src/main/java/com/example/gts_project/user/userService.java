d35a3027af8e1b768a00b386af15a63bae62b919
sg_joo_ <joo38219@gmail.com>
2026-01-02 18:01:55 +0900

    back/VODAO: making userService

---
+++ src/main/java/com/example/gts_project/user/userService.java
@@ -0,0 +1,17 @@
+package com.example.gts_project.user;
+
+import java.util.List;
+
+public interface userService {
+
+    public int saveUser(userVO userVO);
+    public int updateUser(userVO userVO);
+    public int deleteUser(int personId);
+
+    public userVO getUser(int personId);
+    public int getUserCnt();
+    public List<userVO> getUserList();
+    public List<userVO> getUserListByUserType(int userType);
+
+    // public int getTodayUserCalories();
+}
